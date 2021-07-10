628bd7f1529c6ceed7b2c4532773b46411dde0ae
nikoloz.kvachantiradze17b@geolab.edu.ge <nikoloz.kvachantiradze17b@geolab.edu.ge>
2021-07-10 19:01:54 +0400

    redact navbare section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
-import sectionimage from './img/size_p_5_0.jpg';
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>my react app</h1>
-      <h2>authot: niko niko</h2>
-      <img className='section-bg' src={sectionimage} alt="section-image" />
-    </div>
+      <Navbar />
+    </div> 
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-script-url */
+import React from 'react'
+import logo from '../img/logo.svg'
+import {
+    BrowserRouter as Router, Link} from "react-router-dom";
+
+export default function Navbar() {
+
+    return (
+        <>
+        <Router>
+         <div className='navbar-header d-flex container'>
+            <div className="logo-header d-flex" >
+            <Link to="/">
+                <img className='logo' src={logo}  alt="logo"/>
+            </Link>
+                <p className='header-title'>Build Software to Test Software</p>
+            </div>
+                <nav className='navbar'>
+                <ul className='navbar-ul d-flex'>
+                    <li className='navbar-li'>
+                        <Link to="javascript:void(0)"> <i className="fas fa-search "></i>search</Link>
+                    </li>
+                    <li className='navbar-li'>
+                        <Link to="javascript:void(0)">@ subscribe</Link>
+                    </li>
+                    <li className='navbar-li'>
+                        <Link to="javascript:void(0)"> <i className="fas fa-envelope"></i>Contact us</Link>
+                    </li>
+                </ul>
+            </nav>
+         </div>
+        </Router>
+        </>
+    )
+}
